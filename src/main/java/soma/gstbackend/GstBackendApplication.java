76ea0c5e6fe88a50074dbbd73f74954381526b7d
cxz541@gamil.com <cxz541@gmail.com>
2022-07-30 20:29:45 +0900

    Initial commit

---
+++ src/main/java/soma/gstbackend/GstBackendApplication.java
@@ -0,0 +1,13 @@
+package soma.gstbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GstBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soma/gstbackend/GstBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package soma.gstbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
