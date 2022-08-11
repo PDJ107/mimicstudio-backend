7e21e370e75d2cd21df5215fb51c2e1444b5ac55
박동주 <cxz541@gmail.com>
2022-08-11 22:05:05 +0900

    Fix: Juit5로 변경

--- src/test/java/soma/gstbackend/service/ItemServiceTest.java
+++ src/test/java/soma/gstbackend/service/ItemServiceTest.java
@@ -1,11 +1,8 @@
 package soma.gstbackend.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import soma.gstbackend.dto.ItemRequestDto;
 import soma.gstbackend.entity.Category;
@@ -13,9 +10,8 @@ import soma.gstbackend.entity.Item;
 
 import javax.persistence.EntityManager;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class ItemServiceTest {
