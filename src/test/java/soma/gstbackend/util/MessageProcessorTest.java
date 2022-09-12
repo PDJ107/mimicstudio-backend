73b2941cff2f0327113e1b8519ec7ee768791ec6
박동주 <cxz541@gmail.com>
2022-09-12 20:43:28 +0900

    Fix: 테스트 비활성화

--- src/test/java/soma/gstbackend/util/MessageProcessorTest.java
+++ src/test/java/soma/gstbackend/util/MessageProcessorTest.java
@@ -1,5 +1,6 @@
 package soma.gstbackend.util;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ class MessageProcessorTest {
     @Autowired MessageProcessor messageProcessor;
 
     @Test
+    @Disabled
     void send() throws Exception {
         MessageForm messageForm = new ItemMessageForm(0L, "0/20220823082030/");
         messageProcessor.send(messageForm);
