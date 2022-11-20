b87cd645d8758d36d321384ff50bb6c037cc81be
박동주 <cxz541@gmail.com>
2022-11-20 13:33:01 +0900

    Add: 아이템 상태 'failed' 추가

--- src/main/java/soma/gstbackend/enums/ItemStatus.java
+++ src/main/java/soma/gstbackend/enums/ItemStatus.java
@@ -1,5 +1,5 @@
 package soma.gstbackend.enums;
 
 public enum ItemStatus {
-    ready, enqueue, generating, start_colmap, start_nerf, complete
+    ready, enqueue, generating, start_colmap, start_nerf, complete, failed
 }
